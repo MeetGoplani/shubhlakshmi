73f6b061a8b45801db2af2b93abdd014177072d0
MeetGoplani <meet.goplani@nerdshouse.com>
2025-03-05 10:08:15 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Ensure this array is empty or does not include your CSS file
+    },
+  },
 })
